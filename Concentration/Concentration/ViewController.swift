8a4bc48c1b0b06ada340c59f627a31bca000db7b
dlim303 <deli4631@colorado.edu>
2019-08-30 17:09:50 -0600

    initial commitment

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Dennis Lim on 8/30/19.
+//  Copyright © 2019 Dennis Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBAction func touchCard(_ sender: UIButton) {
+        print("agh! a ghost")
+    }
+    
+}
+
